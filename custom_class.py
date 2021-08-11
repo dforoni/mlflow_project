e99f94ee66400be20cb8ccb90d59be1e362025fd
dforoni <deborah.foroni@gmail.com>
2021-08-11 20:25:08 -0300

    desenvolvendo o pipeline com custom class

---
+++ custom_class.py
@@ -0,0 +1,16 @@
+# Custom Transformer Class
+
+'''In order to demonstrate the use of a custom transformer we will create a new feature
+   based upon the ratio of the resting blood pressure to the maximum blood pressure.
+  This feature will be created as a new class and saved into a separate file 
+  so it can be output to the MLflow tracking server to be used during deployment
+   in combination with the saved model.'''
+ 
+
+class NewFeatureTransformer(BaseEstimator, TransformerMixin):
+        def fit(self, x, y=None):
+            return self
+        def transform(self, x):
+            x['ratio'] = x['thalach']/x['trestbps']
+            x = pd.DataFrame(x.loc[:, 'ratio'])
+            return x.values
